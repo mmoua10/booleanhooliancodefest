ba73576ed4e1580c1118136fe905128452a59cdb
aoehlerewu <166942126+aoehlerewu@users.noreply.github.com>
2024-04-13 11:14:58 -0700

    Update car.java

--- javaCode/car.java
+++ javaCode/car.java
@@ -1,4 +1,8 @@
 package javaCode;
 
-public interface car {
+public interface List<T> car {
+
+    boolean add(T element) throws NullPointerException;
+    T get(int index) throws IndexOutOfBoundsException;
+    boolean update(int index, T element) throws IndexOutOfBoundsException, NullPointerException;
 }
